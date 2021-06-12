f2170a4322862d74d99d7b7e235853a75b8e505e
JuanButronC <jon4589@hotmail.com>
2021-06-12 15:16:46 -0500

    Base de Aplicación

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+include 'templates/header.html';
+?>
+<?php
+    //include the main view
+    
+?>
+<div class="container" style="height:1000px">
+</div>
+<?php
+include 'templates/footer.html';
+?>
