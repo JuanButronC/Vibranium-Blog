894102f05f1b380a3b5882a86fe42851fab32ccf
Brandon Rdgz <brandon.rdgz@gmail.com>
2021-06-17 22:34:32 -0500

    Adición de index.php para la redirección a la página de inicio

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
-include 'templates/header.php';
-?>
-<?php
-    //include the main view
-    
-?>
-<div class="container" style="height:1000px">
-</div>
-<?php
-include 'templates/footer.html';
+    session_start();
+    $_SESSION["dirRaiz"] = getcwd() . "/";
+
+    header("location: modulos/home/home.php");
 ?>
